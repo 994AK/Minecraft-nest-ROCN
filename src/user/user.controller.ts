8fb7257d6923453f8ab4ac5a87730826fb675ec5
994AK <1505398065@qq.com>
2023-01-04 19:25:57 +0800

    1、数据库模型更新， 2、登陆接口,注册接口

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,25 @@
-import { Controller,Post,Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query } from '@nestjs/common';
 
 import { UserService } from './user.service';
 @Controller('user')
 export class UserController {
-  constructor(
-    private readonly userService: UserService,
-  ) {}
+  constructor(private readonly userService: UserService) {}
 
-  @Post()
+  @Post('signup')
   async signupUser(
-    @Body() userData: { name: string; email: string; password: string},
-  ){
+    @Body() userData: { name: string; email: string; password: string },
+  ) {
     return this.userService.createUser(userData);
   }
 
+  @Post('login')
+  async loginUser(@Body() userData: { name: string; password: string }) {
+    return this.userService.loginUser(userData);
+  }
+
+  //查询用户信息
+  @Get('findUserById')
+  async findUserById(@Query() query: { id: string }) {
+    return this.userService.findUserById(query);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from "../prisma.service";
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
+  //用户注册
   async createUser(data) {
     // 邮箱不能为空
     if (!data.email) {
       return {
         code: '2',
         data: null,
-        msg: '邮箱不能为空'
-      }
+        msg: '邮箱不能为空',
+      };
     }
 
     // 用户名不能为空
@@ -20,19 +21,16 @@ export class UserService {
       return {
         code: '2',
         data: null,
-        msg: '用户名不能为空'
-      }
+        msg: '用户名不能为空',
+      };
     }
 
     // 先查询用户是否存在
     const getUser = await this.prisma.user.findMany({
       where: {
-        OR: [
-          { email : data.email},
-          { name : data.name}
-        ]
+        OR: [{ email: data.email }, { name: data.name }],
       },
-    })
+    });
 
     if (!getUser.length) {
       const userCreate = await this.prisma.user.create({
@@ -42,16 +40,86 @@ export class UserService {
       return {
         code: '1',
         data: userCreate,
-        msg: '添加用户成功'
-      }
+        msg: '添加用户成功',
+      };
     } else {
       return {
         code: '2',
         data: null,
-        msg: '用户已存在'
-      }
+        msg: '用户已存在',
+      };
+    }
+  }
+
+  //用户登陆
+  async loginUser(data) {
+    if (!data.name) {
+      return {
+        code: '2',
+        data: null,
+        msg: '用户名不能为空',
+      };
     }
 
+    if (!data.password) {
+      return {
+        code: '2',
+        data: null,
+        msg: '密码不能为空',
+      };
+    }
 
+    const login = await this.prisma.user.findMany({
+      where: {
+        AND: [{ name: data.name }, { password: data.password }],
+      },
+    });
+
+    if (login.length === 0) {
+      return {
+        code: '2',
+        data: null,
+        msg: '请检查下用户账号密码是否正确?',
+      };
+    }
+
+    // 删除密码信息
+    delete login[0]?.password;
+    // 删除邮箱信息
+    delete login[0]?.email;
+
+    return {
+      code: '1',
+      data: login[0],
+      msg: '登陆成功',
+    };
+  }
+
+  //查询用户信息
+  async findUserById(query) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: Number(query.id),
+      },
+    });
+
+    if (!user) {
+      return {
+        code: '2',
+        data: null,
+        msg: '没有这个人',
+      };
+    }
+
+    // 删除密码信息
+    delete user?.password;
+    // 删除邮箱信息
+    delete user?.email;
+
+    return {
+      code: '1',
+      data: user,
+      msg: '查询成功',
+    };
   }
 }
